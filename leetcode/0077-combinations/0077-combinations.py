67b5894ea2bedd9c8fc5dd9470851d7822ba25cb
Halleluya Mulugeta <98106633+halleygz@users.noreply.github.com>
2026-03-25 21:29:57 +0300

    Add solution for Combinations

---
+++ leetcode/0077-combinations/0077-combinations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        path = []
+
+        def dfs(start):
+            if len(path) == k:
+                res.append(path[:])
+                return
+
+            need = k - len(path)
+            for i in range(start, n - need + 2):
+                path.append(i)
+                dfs(i + 1)
+                path.pop()
+
+        dfs(1)
+        return res
